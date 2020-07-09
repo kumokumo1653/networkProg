007e6a1e5344f1aada2f69045a80d372354399d8
kumokumo1653 <fjsda526hdbs@gmail.com>
2020-07-09 10:08:05 +0900

    エラー対応

--- cal/main.rb
+++ cal/main.rb
@@ -21,38 +21,46 @@ get '/:year/:month' do
     date = Date.today
     @y = params[:year]
     @m = params[:month]
-    if date.year == @y.to_i
-        if date.month == @m.to_i
-            @nowFlag = true
+    if @y.to_i < 1 || (@m.to_i < 1 && @m.to_i > 12) 
+        erb :error
+    else
+        if date.year == @y.to_i
+            if date.month == @m.to_i
+                @nowFlag = true
+            else 
+                @nowFlag = false
+            end
         else 
             @nowFlag = false
         end
-    else 
-        @nowFlag = false
+        if @nowFlag 
+            @d = date.day
+        else
+            @d = 0
+        end
+        $calendar.setYear(@y.to_i)
+        $calendar.setMonth(@m.to_i)
+        @cal = $calendar.createCal
+        @info,@title = $calendar.createInfo(@cal,@d)
+        erb :calmonth
     end
-    if @nowFlag 
-        @d = date.day
-    else
-        @d = 0
-    end
-    $calendar.setYear(@y.to_i)
-    $calendar.setMonth(@m.to_i)
-    @cal = $calendar.createCal
-    @info,@title = $calendar.createInfo(@cal,@d)
-    erb :calmonth
 end
 
 get'/:year' do
     date = Date.today
     @y = params[:year]
-    @nowFlag = false
-    $calendar.setYear(@y.to_i)
-    @cal = $calendar.createCalYear
-    if(@y.to_i == date.year)
-        @info,@title = $calendar.createInfoYear(@cal,date.month,date.day)
-    else 
-        @info,@title = $calendar.createInfoYear(@cal,0,0)
+    if(@y.to_i < 1)
+        erb :error
+    else
+        @nowFlag = false
+        $calendar.setYear(@y.to_i)
+        @cal = $calendar.createCalYear
+        if(@y.to_i == date.year)
+            @info,@title = $calendar.createInfoYear(@cal,date.month,date.day)
+        else 
+            @info,@title = $calendar.createInfoYear(@cal,0,0)
+        end
+        erb :calyear
     end
-    erb :calyear
 end
 
