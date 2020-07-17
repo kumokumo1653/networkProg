e5f8b58f8f86e2046657e150253ee732c7b91831
kumokumo1653 <fjsda526hdbs@gmail.com>
2020-07-17 14:50:36 +0900

    comp teltel

--- teltel/dbtest.rb
+++ teltel/dbtest.rb
@@ -12,3 +12,18 @@ get '/' do
     @s = Student.all
     erb :index
 end
+
+post '/new' do
+    s = Student.new
+    s.id = params[:id]
+    s.name = params[:name]
+    s.phonenum = params[:tel]
+    s.save
+    redirect '/'
+end
+
+delete '/del' do
+    s = Student.find(params[:id])
+    s.destroy
+    redirect '/'
+end
